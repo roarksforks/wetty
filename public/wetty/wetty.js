657bdbaf96ef5ccc09f3caf9292c31f16390b16f
howardroark <howardroark@users.noreply.github.com>
2016-05-12 19:07:05 -0400

    Update wetty.js

--- public/wetty/wetty.js
+++ public/wetty/wetty.js
@@ -36,6 +36,30 @@ socket.on('connect', function() {
         term.prefs_.set('ctrl-c-copy', true);
         term.prefs_.set('ctrl-v-paste', true);
         term.prefs_.set('use-default-window-copy', true);
+        
+        term.prefs_.resetAll();
+        term.prefs_.set('enable-bold', true);
+        term.prefs_.set('enable-bold-as-bright', false);
+        
+        term.prefs_.set('background-color', '#002b36');
+        term.prefs_.set('background-image', '-webkit-linear-gradient(black, rgba(0, 43, 54, 1))');
+        term.prefs_.set('foreground-color', '#839496');
+        term.prefs_.set('cursor-color', 'rgba(131, 148, 150, 0.5)');
+        
+        term.prefs_.set('color-palette-overrides', ['#073642', '#dc322f',
+            '#859900', '#b58900', '#268bd2', '#d33682', '#2aa198', '#eee8d5',
+            '#002b36', '#cb4b16', '#586e75', '#657b83', '#839496', '#6c71c4',
+            '#93a1a1', '#fdf6e3']);
+        
+        term.prefs_.set('font-family', ('"Source Code Pro", ' +
+                             '"DejaVu Sans Mono", "Everson Mono", ' +
+                             '"FreeMono", "Menlo", "Terminal", ' +
+                             'monospace'));
+        term.prefs_.set('font-size', 12);
+        
+        term.prefs_.set('font-smoothing', 'subpixel-antialiased');
+        term.prefs_.set('cursor-blink', true);
+        term.prefs_.set('scrollbar-visible', false);
 
         term.runCommandClass(Wetty, document.location.hash.substr(1));
         socket.emit('resize', {
